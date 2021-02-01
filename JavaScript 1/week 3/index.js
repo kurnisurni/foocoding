193809ec0e7b5e7558fab7a73d9db50baa267be7
Kurnia <kurniasuryani@gmail.com>
2021-02-01 23:39:44 +0100

    hw 3 in progress

---
+++ JavaScript 1/week 3/index.js
@@ -0,0 +1,33 @@
+// // Task 1
+// let myString = "hello,this,is,a,difficult,to,read,sentence";
+// console.log(myString);
+// console.log(myString.length);
+// let newString = myString.replace(/,/g, " ");
+// console.log(newString);
+
+// // Task 2
+// let favoriteAnimals = ["blowfish", "capricorn", "giraffe"];
+// favoriteAnimals.push("turtle");
+// console.log(favoriteAnimals);
+
+// favoriteAnimals.splice(1, 0, "meerkat");
+// console.log(favoriteAnimals);
+// console.log('The array has a length of:', favoriteAnimals.length);
+
+// delete favoriteAnimals[3];
+// console.log(favoriteAnimals);
+
+// console.log('The item you are looking for is at index:', favoriteAnimals.indexOf("meerkat"));
+
+// More JavaScript
+// //Task 1
+// function totalSum(num1, num2, num3) {
+//     return num1 + num2 + num3;
+// }
+// console.log(totalSum(2, 5, 3));
+
+//Task 2
+function colorCar(color) {
+    return `a ${ color } car`;
+}
+console.log(colorCar("red"));
