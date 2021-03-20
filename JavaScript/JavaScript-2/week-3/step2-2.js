e1fa418fe7104478ca150bcaabebfdc65024f2d9
Kurnia <kurniasuryani@gmail.com>
2021-03-21 00:22:35 +0100

    fix 2-2

--- JavaScript/JavaScript-2/week-3/step2-2.js
+++ JavaScript/JavaScript-2/week-3/step2-2.js
@@ -6,23 +6,34 @@ function threeFive(startIndex, stopIndex, threeCallback, fiveCallback) {
     for (let i = startIndex; i <= stopIndex; i++) { numbers.push(i); }
 
     numbers.forEach(number => {
-        sayThree = threeCallback(number);
-        sayFive = fiveCallback(number);
+        // sayThree = threeCallback(number);
+        // sayFive = fiveCallback(number);
 
+        if (number % 3 === 0 && number % 5 === 0) {
+            threeCallback(number);
+            fiveCallback(number);
+        } else if (number % 3 === 0) {
+            threeCallback(number);
+        } else if (number % 5 === 0) {
+            fiveCallback(number);
+        }
     });
-
 }
 
 function sayThree(number) {
-    if (number % 3 == 0) {
-        console.log('Say three!');
-    }
+    // if (number % 3 == 0) {
+    //     console.log('Say three!');
+    // }
+
+    console.log(number + ' is multiplication of three.');
 }
 
 function sayFive(number) {
-    if (number % 5 == 0) {
-        console.log('Say five!');
-    }
+    // if (number % 5 == 0) {
+    //     console.log('Say five!');
+    // }
+
+    console.log(number + ' is multiplication of five.');
 }
 
 threeFive(10, 15, sayThree, sayFive);
